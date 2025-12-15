d8da03b8f25c2c085d895ee70176acc534192f34
Adriano <adrianosenny@gmail.com>
2025-12-15 08:52:39 -0300

    ✨ feat: criar componente Home com estrutura inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
+import { Notices } from "../components/Notices";
+import { Results } from "../components/Results";
+import { Services } from "../components/Services";
+import { Team } from "../components/Team";
+
+export const Home = () => {
+  return (
+    <>
+      <div
+        className="flex flex-col w-full"
+        style={{ minHeight: "calc(100vh + 1px)" }}
+      >
+        <Header />
+
+        <Hero />
+      </div>
+
+      <Services />
+
+      <Results />
+
+      <Team />
+
+      <Notices />
+
+      <Footer color="bg-white"/>
+    </>
+  );
+};
