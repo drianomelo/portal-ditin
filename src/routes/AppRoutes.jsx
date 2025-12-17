3ee2041df1233afddeeba16aab4e134033db6992
Adriano <adrianosenny@gmail.com>
2025-12-17 17:34:57 -0300

    ✨ feat: Adicionar rota para a página da equipe

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Projeto } from "../pages/Projeto";
+import { Team } from "../pages/Team";
 
 export const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projetos/:id" element={<Projeto />} />
+        <Route path="/equipes/:id" element={<Team />} />
       </Routes>
     </BrowserRouter>
   );
